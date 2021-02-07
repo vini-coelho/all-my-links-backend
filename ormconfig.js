88329d1a29226e71791b8726939052347fb8fecc
Vinicius Coelho <vinicius_coelho@live.com>
2021-02-07 00:49:23 -0400

    :hammer: refactoring to production

---
+++ ormconfig.js
@@ -0,0 +1,24 @@
+module.exports = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: process.env.ENV === 'dev',
+  logging: false,
+  entities: [
+    'src/app/models/**/*.ts',
+  ],
+  migrations: [
+    'src/database/migrations/**/*.ts',
+  ],
+  subscribers: [
+    'src/subscriber/**/*.ts',
+  ],
+  cli: {
+    entitiesDir: 'src/app/models',
+    migrationsDir: 'src/database/migrations',
+    subscribersDir: 'src/subscriber',
+  },
+};
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import 'reflect-metadata';
 import cors from 'cors';
 import express from 'express';
