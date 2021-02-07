6291f6eea2afc652a17d77fd6b8ee5c7302b0f64
Vinicius Coelho <vinicius_coelho@live.com>
2021-02-07 00:23:13 -0400

    :rocket: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import 'reflect-metadata';
+import cors from 'cors';
 import express from 'express';
 import routes from './routes';
 import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
