fb42038ad731c74da00fb1faff1b652e96a6de68
Vinicius Coelho <vinicius_coelho@live.com>
2021-02-07 00:24:11 -0400

    :hammer: refactoring

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,7 @@ import LinkController from './app/controllers/LinkController';
 const router = Router();
 
 router.get('/users', UserController.index);
+router.get('/users/user/:username', UserController.showByUsername);
 router.get('/users/:id', UserController.show);
 router.post('/users', UserController.store);
 router.put('/users/:id', UserController.update);
